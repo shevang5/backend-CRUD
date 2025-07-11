d910419cc7c8f2db0a0377736a200b9e9c0af3d6
shevang5 <shevangpalgam@gmail.com>
2025-07-11 23:34:57 +0530

    crud v0

--- server.js
+++ server.js
@@ -11,31 +11,87 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.post('/notes',async (req, res) => {
-    const { title, content } = req.body;
+// app.post('/notes',async (req, res) => {
+//     const { title, content } = req.body;
+//     await noteModel.create({
+//         title,content
+//     })
+//     res.json({
+//         message:"notes created sucessfully"
+//     })
+
+//     console.log(req.body);
+// })
+
+app.post("/notes", async(req,res)=>{
+    const {title,content} = req.body;
     await noteModel.create({
-        title,content
+        title, content
     })
     res.json({
         message:"notes created sucessfully"
     })
-
-    console.log(req.body);
 })
 
-app.get("/notes",async (req,res)=>{
-    const notes = await noteModel.find()
-    res.json({
-        message:"notes fetch sucessfuly",
-        notes
+// app.get("/notes",async (req,res)=>{
+//     const notes = await noteModel.find()
+//     res.json({
+//         message:"notes fetch sucessfuly",
+//         notes
         
+//     })
+// })
+
+app.get("/notes", async(req,res)=>{
+    const notes = await noteModel.find();
+    res.json({
+        message:"notes fetch sucessfully",
+        notes
     })
 })
 
-app.delete("/notes/:id",async(req,res)=>{
-    const noteid = req.params.id
+// app.patch("/notes/:id",async(req,res)=>{
+//     const noteid = req.params.id
+//     const {title,content} = req.body
+//     await noteModel.findOneAndUpdate({
+//         _id: noteid
+//     },{
+//         title:title,
+//         content:content
+//     })
+//     res.json({
+//         message:"note updated sucessfully"
+//     })
+// })
+app.patch("/notes/:id",async(req,res)=>{
+    const noteid = req.params.id;
+    const {title,content} = req.body
+    await noteModel.findOneAndUpdate({
+        _id:noteid
+    },{
+        title:title,
+        content:content
+    })
+    res.json({
+        message:"note update sucessfully"
+    })
+})
+
+
+// app.delete("/notes/:id",async(req,res)=>{
+//     const noteid = req.params.id
+//     await noteModel.findOneAndDelete({
+//         _id : noteid
+//     })
+//     res.json({
+//         message:"note deleted sucessfully"
+//     })
+// })
+
+app.delete("/notes/:id", async(req,res)=>{
+    const noteid = req.params.id;
     await noteModel.findOneAndDelete({
-        _id : noteid
+        _id:noteid
     })
     res.json({
         message:"note deleted sucessfully"
@@ -43,19 +99,6 @@ app.delete("/notes/:id",async(req,res)=>{
 })
 
 
-app.patch("/notes/:id",async(req,res)=>{
-    const noteid = req.params.id
-    const {title,content} = req.body
-    await noteModel.findOneAndUpdate({
-        _id: noteid
-    },{
-        title:title,
-        content:content
-    })
-    res.json({
-        message:"note updated sucessfully"
-    })
-})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');  
--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
+const { title } = require("process");
 
+
+// const noteSchema = new mongoose.Schema({
+//     title:String,
+//     content:String
+// });
+
+// const noteModel = mongoose.model("note",noteSchema);
+
+// module.exports = noteModel;
 
 const noteSchema = new mongoose.Schema({
     title:String,
     content:String
 });
 
-const noteModel = mongoose.model("note",noteSchema);
+const noteModel = mongoose.model("notes", noteSchema)
 
 module.exports = noteModel;
